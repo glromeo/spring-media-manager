7c4f21715e8b0a7e5da3203a9c348f5db91cb946
Gianluca Romeo <glromeo@gmail.com>
2019-04-27 11:59:19 +0100

    improvements to subtitles

--- src/main/java/org/codebite/springmediamanager/media/TracksService.java
+++ src/main/java/org/codebite/springmediamanager/media/TracksService.java
@@ -8,11 +8,12 @@ import fr.noop.subtitle.model.SubtitleParsingException;
 import fr.noop.subtitle.srt.SrtObject;
 import fr.noop.subtitle.srt.SrtParser;
 import fr.noop.subtitle.vtt.VttWriter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.xmlrpc.XmlRpcException;
 import org.codebite.springmediamanager.data.Movie;
-import org.codebite.springmediamanager.data.mongodb.TracksRepository;
 import org.codebite.springmediamanager.data.tmdb.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -20,9 +21,13 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.io.*;
-import java.net.MalformedURLException;
+import java.lang.reflect.Proxy;
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.zip.GZIPInputStream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -32,23 +37,58 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 
 @Service
+@Slf4j
 public class TracksService {
-
-    @Autowired
-    private TracksRepository tracksRepository;
 
     private OpenSubtitlesClient openSubtitlesClient;
 
+    @Value("${connect.retry.delay:15}")
+    private int connectRetryDelay = 15;
+
+    public TracksService() {
+        connect();
+    }
+
+    ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+    private void connect() {
+        try {
+            URL serverUrl = new URL("https", "api.opensubtitles.org", 443, "/xml-rpc");
+            log.info("connecting to {}", serverUrl.toExternalForm());
+            this.openSubtitlesClient = new OpenSubtitlesClientImpl(serverUrl);
+            log.info("connected");
+        } catch (Exception e) {
+            log.warn("failed to connect, will retry in {} minutes", connectRetryDelay);
+            this.openSubtitlesClient = (OpenSubtitlesClient) Proxy.newProxyInstance(
+                OpenSubtitlesClient.class.getClassLoader(),
+                new Class[]{OpenSubtitlesClient.class},
+                (proxy, method, args) -> {
+                    if (method.getName().equals("searchSubtitles")) {
+                        return Collections.emptyList();
+                    }
+                    if (method.getName().equals("isLoggedIn")) {
+                        return false;
+                    }
+                    return null;
+                });
+            scheduler.schedule(this::connect, connectRetryDelay, TimeUnit.MINUTES);
+        }
+    }
+
     @PostConstruct
-    private void login() throws MalformedURLException, XmlRpcException {
-        URL serverUrl = new URL("https", "api.opensubtitles.org", 443, "/xml-rpc");
-        openSubtitlesClient = new OpenSubtitlesClientImpl(serverUrl);
+    private void login() throws XmlRpcException {
         openSubtitlesClient.login("glromeo", "lilith", "en", "TemporaryUserAgent");
     }
 
     @PreDestroy
     private void logout() throws XmlRpcException {
         openSubtitlesClient.logout();
+    }
+
+    private void ensureLoggedIn() throws XmlRpcException {
+        if (!openSubtitlesClient.isLoggedIn()) {
+            login();
+        }
     }
 
     public ServerInfo openSubtitlesInfo() throws XmlRpcException {
@@ -65,11 +105,12 @@ public class TracksService {
 
     public List<SubtitleInfo> findSubtitles(Movie movie, String lang) throws XmlRpcException {
         String imdbId = movie.imdbId.substring(2);
+        ensureLoggedIn();
         List<SubtitleInfo> subtitles = openSubtitlesClient.searchSubtitles(lang, imdbId);
         return subtitles;
     }
 
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
     @PostConstruct
     public void initRestTemplate() {
@@ -77,6 +118,7 @@ public class TracksService {
     }
 
     public String get(SubtitleInfo subtitleInfo) throws IOException, SubtitleParsingException {
+
         String downloadLink = subtitleInfo.getDownloadLink();
         ResponseEntity<byte[]> track = restTemplate.getForEntity(downloadLink, byte[].class);
 
--- ui/src/media/MediaPlayer.jsx
+++ ui/src/media/MediaPlayer.jsx
@@ -1,12 +1,17 @@
-import React, {useEffect, useState} from 'react';
-import {Player} from "video-react";
+import React, {useEffect, useRef, useState} from 'react';
+import {ControlBar, Player} from "video-react";
 import {playbackMedia} from "../redux/actions";
 import {connect} from "react-redux";
 import {withRouter} from "react-router";
 import {HTTP_HEADERS} from "../util/constants";
+import {languages} from "../util/iso639";
 
 import "./MediaPlayer.scss";
-import {languages} from "../util/iso639";
+
+function CC() {
+    console.log("CC");
+    return <div/>
+}
 
 export function MediaPlayer({match, dispatch, plain = true}) {
 
@@ -31,7 +36,16 @@ export function MediaPlayer({match, dispatch, plain = true}) {
         });
     }, [movieId]);
 
-    const [trackUrl, setTrackUrl] = useState();
+    const [subtitle, setSubtitle] = useState();
+
+    const startTime = localStorage.getItem(`movie[${movieId}].currentTime`);
+
+    function onTimeUpdate(event) {
+        const currentTime = event.currentTarget.currentTime;
+        localStorage.setItem(`movie[${movieId}].currentTime`, currentTime);
+    }
+
+    const ccRef = useRef(null);
 
     if (movie) {
         return <div className="MediaPlayer" style={
@@ -46,11 +60,15 @@ export function MediaPlayer({match, dispatch, plain = true}) {
             <div className="PlayerContainer">
                 <Player playsInLine={true} autoPlay={false}
                         poster={`/api/poster/${movie.id}/large`}
-                        src={`/api/videos/${encodePath(media)}`}>
-                    {trackUrl && <track kind="captions" srcLang="en-US" label="English" default src={trackUrl}/>}
+                        src={`/api/videos/${encodePath(media)}`}
+                        startTime={startTime}
+                        onTimeUpdate={onTimeUpdate}>
+                    <ControlBar>
+                        <Subtitles order={7} selected={subtitle} movie={movie} onChange={setSubtitle}/>
+                    </ControlBar>
+                    {subtitle && <track kind="captions" srcLang="en-US" label="English" default src={subtitle.src}/>}
                 </Player>
             </div>
-            <Subtitles movie={movie} onChange={setTrackUrl}/>
         </div>
     } else {
         return <div>Loading...</div>
@@ -61,10 +79,28 @@ function encodePath({path}) {
     return path.substring(3).split("\\").map(p => encodeURIComponent(p)).join("/");
 }
 
-function Subtitles({movie, onChange}) {
+/**
+ * TODO: Investigate continuous re-rendering
+ *
+ * @param selected
+ * @param movie
+ * @param onChange
+ * @returns {*}
+ * @constructor
+ */
+
+function Subtitles({selected, movie, onChange}) {
     const [isOpen, setOpen] = useState();
     const [subtitles, setSubtitles] = useState([]);
+
+    const dropupRef = useRef();
+
+    function closeDropup(e) {
+        if (!dropupRef.current.contains(e.target) && isOpen) setOpen(false);
+    }
+
     useEffect(() => {
+
         fetch(`/api/tracks/${movie.id}/subtitles`).then(response => response.json()).then(list => {
             const maxDownloadsNo = list.reduce((value, subtitle) => Math.max(value, Number(subtitle.downloadsNo)), 0);
             setSubtitles(list.map(subtitle => ({
@@ -75,6 +111,12 @@ function Subtitles({movie, onChange}) {
                 subtitle
             })));
         }).catch(console.error);
+
+        document.addEventListener("mousedown", closeDropup, false);
+
+        return function () {
+            document.removeEventListener("mousedown", closeDropup, false);
+        }
     }, []);
 
     function colorClass(popularity) {
@@ -90,28 +132,32 @@ function Subtitles({movie, onChange}) {
     }
 
     async function selectSubtitle(subtitle) {
-        const src = `/api/tracks/${movie.id}`;
-        await fetch(src, {
-            method: "PUT",
-            body: JSON.stringify(subtitle),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        onChange(src);
+        if (!subtitle.src) {
+            const src = `/api/tracks/${movie.id}`;
+            await fetch(src, {
+                method: "PUT",
+                body: JSON.stringify(subtitle),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            subtitle.src = src;
+        }
+        onChange(subtitle === selected ? null : subtitle);
         setOpen(false);
     }
 
     return (
-        <div className="dropdown" style={{position: "absolute", top: 70, left: 20}}>
-            <button className="btn btn-secondary dropdown-toggle" type="button" onClick={() => setOpen(!isOpen)}>
-                Subtitles
-            </button>
-            <div className="dropdown-menu" style={{display: isOpen && "block"}}>
+        <div ref={dropupRef} className="Subtitles dropup video-react-control video-react-button">
+            <i className="Toggle fa fa-cc fa-2x m-2"
+               style={{color: selected ? "white" : "rgba(115, 133, 159, 0.5)"}}
+               onClick={() => setOpen(!isOpen)}/>
+            <div className="dropdown-menu dropdown-menu-right text-white" style={{display: isOpen && "block"}}>
                 {subtitles.sort((a, b) => {
                     return a.popularity < b.popularity ? 1 : a.popularity > b.popularity ? -1 : 0;
                 }).map(({key, popularity, title, subtitle, languageCode}) => (
-                    <button key={key} className="dropdown-item d-flex" type="button"
+                    <button key={key} type="button"
+                            className={"dropdown-item d-flex " + (selected === subtitle && "active")}
                             onClick={() => selectSubtitle(subtitle)}>
                         <span className={`badge badge-pill badge-${colorClass(popularity)}`}
                               style={{
@@ -124,6 +170,7 @@ function Subtitles({movie, onChange}) {
                     </button>
                 ))}
             </div>
+            <div className="Angle" style={{display: isOpen ? "block" : "none"}}/>
         </div>
     )
 }
